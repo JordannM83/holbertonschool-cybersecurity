f376c80897e2d250ce44a9a2af87c6f45b49516d
JordannM83 <jordann.miso@gmail.com>
2026-08-06 15:25:41 +0200

    replace passwd command with usermod for locking users in 5-cleanup.sh

--- linux_security/1x01_shell_ops/5-cleanup.sh
+++ linux_security/1x01_shell_ops/5-cleanup.sh
@@ -2,7 +2,7 @@
 INPUT_FILE="$1"
 while IFS= read -r username || [ -n "$Username"]; do
     if id "$username" &>/dev/null; then
-        passwd -l "$username" &>/dev/null
+        usermod -L "$username" &>/dev/null
         echo "User $username locked"
     else
         echo "User $username not found"
