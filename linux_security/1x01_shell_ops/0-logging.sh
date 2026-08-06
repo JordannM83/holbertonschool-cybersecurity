e418c2e7be35b1bd065e2d677cd74bcaa1875165
JordannM83 <jordann.miso@gmail.com>
2026-08-06 09:46:08 +0200

    task 0

--- linux_security/1x01_shell_ops/0-logging.sh
+++ linux_security/1x01_shell_ops/0-logging.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-echo "Starting Task" >> $1 2>&1
-echo "Doing Work" >> $1 2>&1
-echo "Error: Work Failed" >> $1 2>&1
+exec &>"$1" && echo "Starting Task" && echo "Doing Work" && echo "Error: Work Failed" >&2
