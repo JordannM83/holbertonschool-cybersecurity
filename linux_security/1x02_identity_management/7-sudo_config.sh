67c5f39822d30aba50ca793cdf2587be176ade68
JordannM83 <jordann.miso@gmail.com>
2026-08-08 17:23:00 +0200

    fix: update sudoers file path to use a static 'junior' user

--- linux_security/1x02_identity_management/7-sudo_config.sh
+++ linux_security/1x02_identity_management/7-sudo_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 USERNAME="$1"
-FILE="/etc/sudoers.d/$USERNAME"
+FILE="/etc/sudoers.d/junior"
 echo "$USERNAME ALL=(ALL) /usr/bin/systemctl restart apache2, /usr/bin/journalctl" > "$FILE"
 chmod 0440 "$FILE"
 visudo -c -f "$FILE"
