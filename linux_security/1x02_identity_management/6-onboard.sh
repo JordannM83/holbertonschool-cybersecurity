d04819e3d8c3ec329333893341d83c80569c7022
JordannM83 <jordann.miso@gmail.com>
2026-08-08 17:05:36 +0200

    add onboarding script for user management

---
+++ linux_security/1x02_identity_management/6-onboard.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+USERNAME="$1"
+useradd -m -s /bin/bash "$USERNAME"
+passwd -l $1
+USER_HOME="/home/$USERNAME"
+SSH_DIR="$USER_HOME/.ssh"
+AUTH_KEYS="$SSH_DIR/authorized_keys"
+mkdir -p "$SSH_DIR"
+chmod 700 "$SSH_DIR"
+echo "$2" >> "$AUTH_KEYS"
+chmod 600 "$AUTH_KEYS"
+chown -R "$USERNAME:$USERNAME" "$SSH_DIR"
