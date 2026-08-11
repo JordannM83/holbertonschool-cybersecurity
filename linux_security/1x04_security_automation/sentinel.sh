565bfb6e3a8b56bc626a8a08b5693e852d63f126
JordannM83 <jordann.miso@gmail.com>
2026-08-11 11:26:19 +0200

    refactor: encapsulate service checking logic in a function in sentinel.sh

--- linux_security/1x04_security_automation/sentinel.sh
+++ linux_security/1x04_security_automation/sentinel.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 [ -f "sentinel.conf" ] || exit 1; source "sentinel.conf"; [ -n "${SERVICES+x}" ] && [ -n "${FILES_TO_WATCH+x}" ] || exit 1
-for service in "${SERVICES[@]}"; do
-    if pgrep -f "${service}" &> /dev/null 2>&1; then
-        logger "OK: ${service} is running"
-    else 
-        eval "${service}"
-        if pgrep -f "${service}" &> /dev/null 2>&1; then
-            logger "FIXED: Restarted ${service}"
-        else
-            logger "ERROR: ${service} start fails"
+
+check_services () {
+    for service in "${SERVICES[@]}"; do
+        if pgrep -f "${service}" &> /dev/null; then
+            echo "OK: ${service} is running"
+        else 
+            eval "${service}"
+            if pgrep -f "${service}" &> /dev/null; then
+                echo "FIXED: Restarted ${service}"
+            else
+                echo "ERROR: ${service} start fails" >2
+            fi
         fi
-    fi
-done
+    done
+}
+
+check_services
