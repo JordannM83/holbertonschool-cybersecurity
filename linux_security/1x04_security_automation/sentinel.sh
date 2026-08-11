50314f87024ad10b6ce178bf2184f6693cc7e5ac
JordannM83 <jordann.miso@gmail.com>
2026-08-11 11:09:23 +0200

    feat: implement service monitoring logic in sentinel.sh

--- linux_security/1x04_security_automation/sentinel.sh
+++ linux_security/1x04_security_automation/sentinel.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 [ -f "sentinel.conf" ] || exit 1; source "sentinel.conf"; [ -n "${SERVICES+x}" ] && [ -n "${FILES_TO_WATCH+x}" ] || exit 1
+for service in "${SERVICES[@]}"; do
+    if pgrep -f "${service}" &> /dev/null 2>&1; then
+        logger "OK: ${service} is running"
+    else 
+        eval "${service}"
+        if pgrep -f "${service}" &> /dev/null 2>&1; then
+            logger "FIXED: Restarted ${service}"
+        else
+            logger "ERROR: ${service} start fails"
+        fi
+    fi
+done
