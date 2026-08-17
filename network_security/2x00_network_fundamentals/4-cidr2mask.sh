27a931cf7ff17f285c3253a3d61894c3ea3b3556
JordannM83 <jordann.miso@gmail.com>
2026-08-17 12:15:03 +0200

    fix: remove newline from CIDR to mask output

--- network_security/2x00_network_fundamentals/4-cidr2mask.sh
+++ network_security/2x00_network_fundamentals/4-cidr2mask.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cidr="$1"; mask=$((0xFFFFFFFF << (32 - cidr))); printf "%d.%d.%d.%d\n" $((mask>>24 & 255)) $((mask>>16 & 255)) $((mask>>8 & 255)) $((mask & 255))
+cidr="$1"; mask=$((0xFFFFFFFF << (32 - cidr))); printf "%d.%d.%d.%d" $((mask>>24 & 255)) $((mask>>16 & 255)) $((mask>>8 & 255)) $((mask & 255))
