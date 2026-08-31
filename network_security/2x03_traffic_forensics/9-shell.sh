1a8c2a877faa46cd2f6879690bafd278827799dd
JordannM83 <jordann.miso@gmail.com>
2026-08-31 14:51:50 +0200

    fix: update tshark command to output destination port for root access detection

--- network_security/2x03_traffic_forensics/9-shell.sh
+++ network_security/2x03_traffic_forensics/9-shell.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tshark -r "$1" -Y 'tcp contains "uid=0" or tcp contains "root"' | awk '{print $5}'
+tshark -r "$1" -Y 'tcp contains "uid=0" or tcp contains "root"' -T fields -e tcp.dstport
