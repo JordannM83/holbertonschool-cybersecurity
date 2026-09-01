816fa06076742426b9d47fc30838667f54a9aa45
JordannM83 <jordann.miso@gmail.com>
2026-09-01 14:16:07 +0200

    fix: correct tshark command syntax in ICMP tunnel and RCE scripts; add skeleton configuration file

--- network_security/2x03_traffic_forensics/8-rce.sh
+++ network_security/2x03_traffic_forensics/8-rce.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#tshark -r "$1" -Y 'frame contains "/bin/sh"' | awk '{pint $1}'
+#tshark -r "$1" -Y 'frame contains "/bin/sh"' | awk '{print $1}'
 tshark -r "$1" .(/bin/sh). -T fields -e frame.number
