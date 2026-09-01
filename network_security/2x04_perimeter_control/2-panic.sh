0c845ef63c2fe8e4bb5f3db4d26c10ba72bf5fad
JordannM83 <jordann.miso@gmail.com>
2026-09-01 17:36:19 +0200

    fix: format nft commands in panic script for readability; add skeleton configuration file

--- network_security/2x04_perimeter_control/2-panic.sh
+++ network_security/2x04_perimeter_control/2-panic.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-nft flush ruleset; nft add table inet filter; nft add chain inet filter input '{ type filter hook input priority 0; policy accept; }'; nft add chain inet filter forward '{ type filter hook forward priority 0; policy accept; }'; nft add chain inet filter output '{ type filter hook output priority 0; policy accept; }'; echo "$(realpath "$0")" | at now + 5 minutes
+nft flush ruleset
+
+nft add table inet filter
+nft add chain inet filter input '{ type filter hook input priority 0; policy accept; }'
+nft add chain inet filter forward '{ type filter hook forward priority 0; policy accept; }'
+nft add chain inet filter output '{ type filter hook output priority 0; policy accept; }'
+
+echo "$(realpath "$0")" | at now + 5 minutes
