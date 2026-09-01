709afbe0239db90e53c1c8de370053786a3687ec
JordannM83 <jordann.miso@gmail.com>
2026-09-01 13:59:24 +0200

    feat: add panic script to configure nftables ruleset

---
+++ network_security/2x04_perimeter_control/2-panic.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+nft flush ruleset; nft add table inet filter; nft add chain inet filter input '{ type filter hook input priority 0; policy accept; }'; nft add chain inet filter forward '{ type filter hook forward priority 0; policy accept; }'; nft add chain inet filter output '{ type filter hook output priority 0; policy accept; }'; echo "$(realpath "$0")" | at now + 5 minutes
