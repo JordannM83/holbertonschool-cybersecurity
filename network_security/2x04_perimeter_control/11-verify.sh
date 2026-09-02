8b7cb56d1b3d6d0237db99f263f325c87474cc20
JordannM83 <jordann.miso@gmail.com>
2026-09-02 11:04:17 +0200

    fix: update verification script to use temporary file for WireGuard configuration

--- network_security/2x04_perimeter_control/11-verify.sh
+++ network_security/2x04_perimeter_control/11-verify.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-scp wg0.conf "$2":/etc/wireguard/wg0.conf
-wg show | wg-quick up wg0
+scp wg0.conf "$1":/tmp/wireguard/wg0.conf
+ssh "$1" 'wg-quick up wg0'
 wg-quick up client
-ping -c 10.200.0.1
+ping -c 4 10.200.0.1
+wg show wg0 latest-handshakes | awk '{print $2}'
