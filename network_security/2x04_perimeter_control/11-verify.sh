7d3a1fb9c04571a7bacf1727fb29f07a4cdb7809
JordannM83 <jordann.miso@gmail.com>
2026-09-02 10:56:44 +0200

    fix: correct scp command syntax in verification script for WireGuard

--- network_security/2x04_perimeter_control/11-verify.sh
+++ network_security/2x04_perimeter_control/11-verify.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-scp "wg0.conf "$2":/etc/wireguard/wg0.conf
-wg-quick up wg0
+scp wg0.conf "$2":/etc/wireguard/wg0.conf
+wg show | wg-quick up wg0
 wg-quick up client
 ping -c 10.200.0.1
