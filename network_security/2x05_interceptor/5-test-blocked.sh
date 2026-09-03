df58c11f6b4f70597fd9a4136a32dee6d6bbc487
JordannM83 <jordann.miso@gmail.com>
2026-09-03 10:42:57 +0200

    feat: update test script to check for 403 status code from blocked domains

--- network_security/2x05_interceptor/5-test-blocked.sh
+++ network_security/2x05_interceptor/5-test-blocked.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -x http://proxy_ip:3128 -o /dev/null -s -w "%{http_code}" http://example.malware.com
+curl -x http://proxy_ip:3128 -o /dev/null -s -w "%{http_code}\n" http://example.malware.com | grep 403
