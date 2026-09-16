63c015f556999dc0d6d4712280cf65db699ecc1c
JordannM83 <jordann.miso@gmail.com>
2026-09-16 13:51:31 +0200

    feat: add script to set up rsyslog configuration and backup

---
+++ security_concepts/3x04_the_watchtower/3-setup_loghost.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+FILE="/etc/rsyslog.conf"
+BACKUP="/etc/backup/rsyslog.conf.backup"
+
+mkdir -p /etc/backup
+
+cp "$FILE" "$BACKUP"
+
+sed -i -E '/UDP.*514/s/^[[:space:]]*#[[:space:]]*//' "$FILE"
+sed -i -E '/TCP.*514/s/^[[:space:]]*#[[:space:]]*//' "$FILE"
+
+rsyslogd -N1
+
+if [ $? -eq 0 ]; then
+    systemctl restart rsyslog
+else
+    echo "Erreur dans la configuration rsyslog."
+    cp "$BACKUP" "$FILE"
+    exit 1
+fi
